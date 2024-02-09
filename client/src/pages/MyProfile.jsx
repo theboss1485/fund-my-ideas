44ee197e7383e2b2a1b4c78943a2c487f630f1b7
Villy Yeoh <yeohvilly@gmail.com>
2024-02-08 19:20:12 -0500

    added something to my profile page

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -1,3 +1,12 @@
 export default function MyProfile() {
-    
+    return (
+        // if logged in function here to redirect you to login?
+        <section className="custom-my-profile-main">
+            <div className="custom-start-project-section">
+
+            </div>
+            <h1>my profile</h1>
+            <button className="btn btn-lg btn-primary">Begin a project!</button>
+        </section>
+    )
 }
